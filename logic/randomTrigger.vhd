50cb68a430699ca0335cf178c64e7153f9b68b95
Mattia Barbanera <mattia.barbanera@pg.infn.it>
2022-05-11 16:38:32 +0200

    Revisit random triggre

--- logic/randomTrigger.vhd
+++ logic/randomTrigger.vhd
@@ -40,28 +40,28 @@ end randomTrigger;
 
 --!@copydoc top_randomTrigger.vhd
 architecture Behavior of randomTrigger is  
-  signal sPRBS32Out      : std_logic_vector(31 downto 0);   --!PRBS32 output
-  signal sBusyCounter    : std_logic_vector(31 downto 0);   --!Counter for trigger pause
-  signal sShaperCounter  : std_logic_vector(31 downto 0);   --!Counter for shaper
-  signal sTrig           : std_logic;                       --!Output trigger
-  signal sSlowClock      : std_logic;                       --!Slow clock for PRBS32
-  signal sFreqDiv        : std_logic_vector(31 downto 0);   --!Slow clock duration
-  signal sFreqDivDelay   : std_logic_vector(31 downto 0);   --!Slow clock duration (1-CLK delay)
-  signal sFreqDivFlag    : std_logic;                       --!iFREQ_DIV[k] /= iFREQ_DIV[k-1]
-  signal sFreqDivRst     : std_logic;                       --!Slow clock reset
+  signal sPRBS32Out   : std_logic_vector(31 downto 0); --!PRBS32 output
+  signal sCountBusy   : std_logic; --!'1': count busy, '0': wait
+  signal sBusyCounter : unsigned(31 downto 0);         --!Counter for trigger pause
+  signal sIntBusy     : std_logic;  --!Internal Busy
+  signal sSlowClock   : std_logic;                     --!Slow clock for PRBS32
+  signal sClockEdge   : std_logic;                     --!Slow clock rising edge
+  signal sFreqDiv     : std_logic_vector(31 downto 0); --!Slow clock duration
+  signal sFreqDivRst  : std_logic;                     --!Slow clock reset
+  signal sEval        : std_logic;                     --!'1': Evaluate value wrt threshold
+  signal sTriggered   : std_logic;  --!Internal trigger signal
+  signal sTrig        : std_logic;  --!Decision wether to assert trigger
   
 begin
   --!Combinatorial assignments
-  sFreqDiv        <= iFREQ_DIV;
-  sFreqDivRst     <= iRST or sFreqDivFlag;
-  oTRIG           <= sTrig;
+  sFreqDivRst     <= iRST;
   oSLOW_CLOCK     <= sSlowClock;
   
   pseudocasual_32bit_value : PRBS32
     port map(
       iCLK       => iCLK,
       iRST       => iRST,
-      iPRBS32_en => sSlowClock,
+      iPRBS32_en => sEval,
       oDATA      => sPRBS32Out
       );
       
@@ -73,74 +73,70 @@ begin
 	port map(
 		iCLK 					    => iCLK,
 		iRST 					    => sFreqDivRst,
-		iEN 					    => '1',
+		iEN 					    => iEN,
 		oCLK_OUT 			    => sSlowClock,
-		oCLK_OUT_RISING 	=> open,
+		oCLK_OUT_RISING 	=> sClockEdge,
 		oCLK_OUT_FALLING 	=> open,
-    iFREQ_DIV         => iFREQ_DIV,
-    iDUTY_CYCLE       => x"00000001"
+    iFREQ_DIV         => sFreqDiv,
+    iDUTY_CYCLE       => iSHAPER_T_ON
 		);
-      
-  --!comparison between threshold and PRBS32 output
+  
+  --!
   comp : process (iCLK)
   begin
-    if (rising_edge(iCLK)) then
-    
-      --!RESET
-      if (iRST = '1') then
-        sTrig <= '0';
-        sShaperCounter <= (others => '0');
-      
+    RE_CLK_IF : if (rising_edge(iCLK)) then
+      RST_IF : if (iRST = '1') then
+        oTRIG         <= '0';
+        sTrig         <= '0';
+        sCountBusy <= '0';
+        sBusyCounter <= (others => '1');
+        sIntBusy <= '0';
       elsif (iEN = '1') then
-      --!default value
-      sShaperCounter <= (others => '0');
-      sBusyCounter   <= (others => '0');
-        --!TRIGGER ON  
-        if (sTrig = '1') then
-          if (sShaperCounter < iSHAPER_T_ON) then
-            sTrig <= '1';
-            sShaperCounter <= sShaperCounter + 1;
+        --Evaluate if trigger shall be generated, if not busy
+        TRIG_EVAL_IF : if (sEval = '1') then
+          if (unsigned(sPRBS32Out) > unsigned(iTHRESHOLD)) then
+            if (sIntBusy = '0' and iEXT_BUSY = '0') then
+              sTrig <= '1';
+            else
+              sTrig <= '0';
+            end if;
           else
             sTrig <= '0';
-            sBusyCounter <= sBusyCounter + 1;
           end if;
-        
-        --!TRIGGER OFF  
-        elsif (sTrig = '0') then
-          if (sBusyCounter < iINT_BUSY and sBusyCounter > 0) then
-            sBusyCounter <= sBusyCounter + 1;
-          elsif (sPRBS32Out > iTHRESHOLD and (iEXT_BUSY = '0')) then
-            sTrig <= '1';
-            sShaperCounter <= sShaperCounter + 1;
-          end if;
-      
-        --!TRIGGER 'X'
+        end if TRIG_EVAL_IF;
+
+        --Generate the internal busy signal
+        BUSY_COUNT_IF : if (sTrig = '1' and sTriggered = '1') then
+          sIntBusy <= '1';
+          sBusyCounter <= (others => '0');
+        elsif (sBusyCounter < (unsigned(iINT_BUSY)-1)) then
+          sIntBusy <= '1';
+          sBusyCounter <= sBusyCounter + 1;
         else
-          sTrig <= '0';
+          sIntBusy <= '0';
+          --sBusyCounter <= (others => '0');
+        end if BUSY_COUNT_IF;
+
+        --Output Trigger
+        if (sTrig = '1') then
+          oTRIG <= sSlowClock;
+        else
+          oTRIG <= '0';
         end if;
-      end if;
-    end if;
+
+      end if RST_IF;
+    end if RE_CLK_IF;
   end process;
-    
-  --!delay iFREQ_DIV value
+
+  sEval <= sClockEdge;
+  --!FFD buffers
   ffd : process (iCLK)
 	begin
 		if rising_edge(iCLK) then
-			sFreqDivDelay <= sFreqDiv;
+			sFreqDiv <= iFREQ_DIV; --!iFREQ_DIV buffer
+      sTriggered    <= sEval;
 		end if;
 	end process;
-  
-  --!chek for iFREQ_DIV changes
-  FREQ_DIV_changes : process (iCLK)
-  begin
-    if rising_edge(iCLK) then
-      if (sFreqDiv /= sFreqDivDelay) then
-        sFreqDivFlag <= '1';
-      else
-        sFreqDivFlag <= '0';
-      end if;
-    end if;
-  end process;
   
  
 end Behavior;
